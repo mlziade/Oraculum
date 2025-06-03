fde61188d605e1d64020306f50ece500441e9073
mlziade <ziade.michel@hotmail.com>
2025-06-03 17:27:52 -0300

    refactor: update Picture model to use ImageField for miniature and add ManyToMany relationship for tags

--- gallery/models.py
+++ gallery/models.py
@@ -6,8 +6,8 @@ class Picture(models.Model):
     description = models.TextField(blank=True, null=True, help_text="Description of the picture")
     created_at = models.DateTimeField(auto_now_add=True, help_text="Date and time when the picture was created")
     updated_at = models.DateTimeField(auto_now=True, help_text="Date and time when the picture was last updated")
-    is_miniature = models.BooleanField(default=False, help_text="Indicates if the picture is a miniature")
-    miniature = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True, related_name='original', help_text="Miniature version of this picture (optional)")    
+    miniature = models.ImageField(upload_to='miniatures/', blank=True, null=True, help_text="Miniature version of this picture (optional)")
+    tags = models.ManyToManyField('Tag', blank=True, related_name='pictures', help_text="Tags associated with this picture")
 
     def __str__(self):
         return self.title
@@ -16,3 +16,15 @@ class Picture(models.Model):
         verbose_name = "Picture"
         verbose_name_plural = "Pictures"
         ordering = ['title']
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True, help_text="Name of the tag")
+    description = models.TextField(blank=True, null=True, help_text="Description of the tag")
+    classification = models.CharField(max_length=50, blank=True, null=True, help_text="Classification of the tag (optional)")
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+        ordering = ['name']
