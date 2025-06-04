f7b3810379b7a09c9b58f9951cd30b20edfd656c
mlziade <ziade.michel@hotmail.com>
2025-06-03 23:54:00 -0300

    add: enhance ProcessingQueue admin to display clickable links to associated Pictures

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 from .models import Picture, Tag, ProcessingQueue, TagClassification
 
 @admin.register(Picture)
@@ -24,7 +26,17 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(ProcessingQueue)
 class ProcessingQueueAdmin(admin.ModelAdmin):
-    list_display = ('id', 'picture', 'status', 'created_at', 'updated_at')
+    list_display = ('id', 'picture_link', 'status', 'created_at', 'updated_at')
     list_filter = ('status', 'created_at', 'updated_at')
     search_fields = ('picture__title',)
     readonly_fields = ('id', 'created_at', 'updated_at')
+    
+    def picture_link(self, obj):
+        """Create a clickable link to the Picture admin page"""
+        if obj.picture:
+            url = reverse('admin:gallery_picture_change', args=[obj.picture.pk])
+            return format_html('<a href="{}">{}</a>', url, obj.picture.title)
+        return '-'
+    
+    picture_link.short_description = 'Picture'
+    picture_link.admin_order_field = 'picture__title'
